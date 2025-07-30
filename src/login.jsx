5167458fcc9af70ba51bfa0d73aa2aff28dfbf9c
hyeongmin90 <nsby8882@naver.com>
2025-07-30 19:55:56 +0900

    로그인 api 연결

--- src/login.jsx
+++ src/login.jsx
@@ -1,21 +1,47 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom"
 import './login.css'
-
+import { admin_login_api, worker_login_api } from "./api";
 
 export function Login() {
     const [role, setRole] = useState("admin");
-    const [loginId, setLoginId] = useState("");
-    const [password, setPassword] = useState("");
     const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+        "loginId": "",
+        "password": ""
+    })
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (!loginId.trim() || !password.trim()) {
+        if (!formData.loginId.trim() || !formData.password.trim()) {
             alert("아이디와 비밀번호를 모두 입력해주세요.");
             return;
         }
-        //로그인 로직
+        try {
+            let response;
+            if (role === "admin") {
+                response = admin_login_api(formData);
+
+            }
+            else if (role === "worker") {
+                response = worker_login_api(formData);
+            }
+            console.log(response);
+        }
+        catch (error) {
+            console.error("로그인 중 오류 발생", error)
+            alert("로그인 오류 발생")
+        }
+
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prev => ({
+            ...prev,
+            [name]: value,
+        }));
     };
 
     const handleSignup = (e) => {
@@ -48,10 +74,11 @@ export function Login() {
                         <label>
                             아이디
                             <input
-                                type="loginId"
-                                value={loginId}
-                                onChange={(e) => setLoginId(e.target.value)}
-                                // required
+                                type="text"
+                                name="loginId"
+                                value={formData.loginId}
+                                onChange={handleChange}
+                                required
                                 className="login-input"
                                 placeholder="아이디를 입력하세요."
                             />
@@ -62,8 +89,9 @@ export function Login() {
                             비밀번호
                             <input
                                 type="password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
+                                name="password"
+                                value={formData.password}
+                                onChange={handleChange}
                                 // required
                                 className="login-input"
                                 placeholder="비밀번호를 입력하세요."
@@ -79,12 +107,11 @@ export function Login() {
                         <button className="login-signup-btn" onClick={handleSignup}>회원가입</button>
                     ) : null}
                 </div>
-            </div>
 
-            <div>
+            </div>
+            <div className="footer">
                 © 2025 Hyundai Motor Company. All rights reserved.
             </div>
-
         </div>
     );
 }
