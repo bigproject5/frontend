73669630cfe3892de1e62b16c7c408ba9be4f59d
김강년 <64246800+ekek54@users.noreply.github.com>
2025-08-04 20:47:59 +0900

    Feat: 추가 Vite 서버 설정 및 API 프록시 구성
    Vite 설정 파일에 서버 포트 및 호스트를 추가하고,
    API 요청을 위한 프록시 설정을 구성했습니다.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    host: true,
+    proxy: {
+      '/api/operation': {
+        target: 'http://localhost:8082',
+        changeOrigin: true,
+        secure: false
+      },
+      '/api/vehicleaudit': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
