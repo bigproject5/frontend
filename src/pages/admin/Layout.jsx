bff3892c330c60ca13116a137d651f70c4916396
김강년 <64246800+ekek54@users.noreply.github.com>
2025-08-06 09:59:34 +0900

    Rename: Layout 및 Sidebar 컴포넌트 위치 변경

--- src/pages/admin/Layout.jsx
+++ src/pages/admin/Layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Sidebar from "./Sidebar";
+import Sidebar from "./Sidebar.jsx";
 import { Box, CssBaseline } from "@mui/material";
 import { Outlet } from "react-router-dom";
 
