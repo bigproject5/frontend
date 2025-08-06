17e49407c19aaafe42356fec1bf5e0247b6e1ee5
김강년 <64246800+ekek54@users.noreply.github.com>
2025-08-06 15:00:50 +0900

    Feat: 추가 관리자 및 작업자 전용 라우트 구현
    관리자와 작업자 전용 페이지에 대한 라우트를 추가하여
    인증된 사용자만 접근할 수 있도록 보호 기능을 구현했습니다.
    각 페이지는 공통 레이아웃을 사용하여 일관된 UI를 제공합니다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,106 @@
 // src/App.jsx
-import React from 'react'
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Login } from "./pages/Login/login.jsx";
-import { Signup } from "./pages/Signup/Signup.jsx";
+import Login from "./pages/Login/login.jsx";
+import Signup from "./pages/Signup/Signup.jsx";
+import WorkerMainContent from "./pages/WorkerMain/WorkerMainContent.jsx";
 import WorkerProfile from "./pages/WokerProfile/WorkerProfile.jsx";
-import AdminNotice from './pages/Notice/AdminNotice.jsx'    // 관리자 대시보드
-import NoticeList from './pages/Notice/NoticeList.jsx'      // 작업자용 - 조회만
-import NoticeDetail from './pages/Notice/NoticeDetail.jsx'  // 관리자용 - 수정/삭제 가능
-import NoticeForm from './pages/Notice/NoticeForm.jsx'
-import WorkerForm from "./pages/Workers/WorkerForm.jsx";
 import WorkerList from "./pages/Workers/WorkerList.jsx";
+import WorkerForm from "./pages/Workers/WorkerForm.jsx";
+import AdminNotice from "./pages/Notice/AdminNotice.jsx";
+import NoticeForm from "./pages/Notice/NoticeForm.jsx";
+import NoticeList from "./pages/Notice/NoticeList.jsx";
+import NoticeDetail from "./pages/Notice/NoticeDetail.jsx";
 import Workerpartdetail from "./pages/WorkerPartDetail/workerpartdetail.jsx";
+import MainLayout from "./components/Layout/MainLayout.jsx";
 import Cartest from "./pages/WorkerPartDetail/cartest.jsx";
-import InspectionDetail from "./pages/admin/InspectionDetail.jsx";
 import Dashboard from "./pages/admin/Dashboard";
+import { AdminRoute, WorkerRoute, ProtectedRoute } from "./components/ProtectedRoute.jsx";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* 인증 관련 페이지 (레이아웃 없음) */}
         <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/worker" element={<WorkerProfile />} />
 
-        <Route path="/admin" element={<AdminNotice />} />
-        <Route path="/notice" element={<AdminNotice />} />
-        <Route path="/admin/write" element={<NoticeForm />} />
-        <Route path="/admin/edit/:id" element={<NoticeForm />} />
-        <Route path="/admin/notice/:id" element={<NoticeDetail />} />
+        {/* 작업자 전용 페이지 */}
+        <Route path="/worker" element={
+          <WorkerRoute>
+            <MainLayout><WorkerMainContent /></MainLayout>
+          </WorkerRoute>
+        } />
+        <Route path="/worker/profile" element={
+          <WorkerRoute>
+            <MainLayout><WorkerProfile /></MainLayout>
+          </WorkerRoute>
+        } />
 
-        <Route path="/NoticeList" element={<NoticeList />} />
-        <Route path="/notice/:id" element={<NoticeDetail />} />
+        {/* 관리자 전용 페이지 */}
+        <Route path="/admin" element={
+          <AdminRoute>
+            <MainLayout><Dashboard /></MainLayout>
+          </AdminRoute>
+        } />
+        <Route path="/admin/dashboard" element={
+          <AdminRoute>
+            <MainLayout><Dashboard /></MainLayout>
+          </AdminRoute>
+        } />
+        <Route path="/admin/workers" element={
+          <AdminRoute>
+            <MainLayout><WorkerList /></MainLayout>
+          </AdminRoute>
+        } />
+        <Route path="/admin/workers/new" element={
+          <AdminRoute>
+            <MainLayout><WorkerForm /></MainLayout>
+          </AdminRoute>
+        } />
+        <Route path="/admin/workers/:workerId" element={
+          <AdminRoute>
+            <MainLayout><WorkerProfile /></MainLayout>
+          </AdminRoute>
+        } />
+        <Route path="/admin/notices" element={
+          <AdminRoute>
+            <MainLayout><AdminNotice /></MainLayout>
+          </AdminRoute>
+        } />
+        <Route path="/admin/notices/new" element={
+          <AdminRoute>
+            <MainLayout><NoticeForm /></MainLayout>
+          </AdminRoute>
+        } />
+        <Route path="/admin/notices/:id/edit" element={
+          <AdminRoute>
+            <MainLayout><NoticeForm /></MainLayout>
+          </AdminRoute>
+        } />
+        <Route path="/admin/audits/:auditId" element={
+          <AdminRoute>
+            <MainLayout><Cartest /></MainLayout>
+          </AdminRoute>
+        } />
 
-        <Route path="/workers" element={<WorkerList />} />
-        <Route path="/workers/register" element={<WorkerForm />} />
-
-        <Route path="/Worker-partdetail/:inspectionId" element={<Workerpartdetail />} />
-        <Route path="/Car-test" element={<Cartest />} />
-
-        <Route path="/admin/dashboard" element={<Dashboard />} />
-        <Route path="/admin/inspections/:inspectionId" element={<InspectionDetail />} />
+        {/* 공통 페이지 (인증 필요) */}
+        <Route path="/notices" element={
+          <ProtectedRoute>
+            <MainLayout><NoticeList /></MainLayout>
+          </ProtectedRoute>
+        } />
+        <Route path="/notices/:id" element={
+          <ProtectedRoute>
+            <MainLayout><NoticeDetail /></MainLayout>
+          </ProtectedRoute>
+        } />
+        <Route path="/inspections/:inspectionId" element={
+          <ProtectedRoute>
+            <MainLayout><Workerpartdetail /></MainLayout>
+          </ProtectedRoute>
+        } />
       </Routes>
     </Router>
   );
