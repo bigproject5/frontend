7b6823d142fc3a0d28fa84328c32c01b58a2d8fc
hyeongmin90 <nsby8882@naver.com>
2025-08-06 09:53:25 +0900

    디렉토리 구조 수정

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import CssBaseline from '@mui/material/CssBaseline'
 import Container from '@mui/material/Container'
 
-import AdminNotice from './notices/AdminNotice'    // 관리자 대시보드
-import NoticeList from './notices/NoticeList'      // 작업자용 - 조회만
-import NoticeDetail from './notices/NoticeDetail'  // 관리자용 - 수정/삭제 가능
-import NoticeForm from './notices/NoticeForm'      // 관리자용 - 등록/수정
+import AdminNotice from './pages/Notice/AdminNotice.jsx'    // 관리자 대시보드
+import NoticeList from './pages/Notice/NoticeList.jsx'      // 작업자용 - 조회만
+import NoticeDetail from './pages/Notice/NoticeDetail.jsx'  // 관리자용 - 수정/삭제 가능
+import NoticeForm from './pages/Notice/NoticeForm.jsx'      // 관리자용 - 등록/수정
 
 function App() {
   return (
